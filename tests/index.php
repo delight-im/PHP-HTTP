bad565f7971f801b41ba02c898bf47e5788d55c7
Marco <marco@delight.im>
2021-10-12 10:55:29 +0200

    Use individual (inline) names/values instead of constants for headers

--- tests/index.php
+++ tests/index.php
@@ -19,26 +19,22 @@ header('Content-type: text/plain; charset=utf-8');
 
 require __DIR__.'/../vendor/autoload.php';
 
-define('TEST_HEADER_NAME', 'X-PHP-HTTP-Test');
-define('TEST_HEADER_VALUE', 42);
-define('TEST_HEADER', TEST_HEADER_NAME.': '.TEST_HEADER_VALUE);
-
-\Delight\Http\ResponseHeader::set(TEST_HEADER_NAME, TEST_HEADER_VALUE);
-assert(\Delight\Http\ResponseHeader::get(TEST_HEADER_NAME) === TEST_HEADER) or exit;
+\Delight\Http\ResponseHeader::set('X-Dd75c0cffe64df82', '535ab646c2f8451a');
+assert(\Delight\Http\ResponseHeader::get('X-Dd75c0cffe64df82') === 'X-Dd75c0cffe64df82: 535ab646c2f8451a') or exit;
 assert(\Delight\Http\ResponseHeader::get('Content-type') === 'Content-type: text/plain; charset=utf-8') or exit;
 assert(\Delight\Http\ResponseHeader::get('Content-type', 'text/p') === 'Content-type: text/plain; charset=utf-8') or exit;
 assert(\Delight\Http\ResponseHeader::get('Content-type', 'text/h') === null) or exit;
 
-\Delight\Http\ResponseHeader::remove(TEST_HEADER_NAME, 'a');
-assert(\Delight\Http\ResponseHeader::get(TEST_HEADER_NAME) === TEST_HEADER) or exit;
+\Delight\Http\ResponseHeader::remove('X-Dd75c0cffe64df82', 'z');
+assert(\Delight\Http\ResponseHeader::get('X-Dd75c0cffe64df82') === 'X-Dd75c0cffe64df82: 535ab646c2f8451a') or exit;
 
-\Delight\Http\ResponseHeader::remove(TEST_HEADER_NAME, substr(TEST_HEADER_VALUE, 0, 4));
-assert(\Delight\Http\ResponseHeader::get(TEST_HEADER_NAME) === null) or exit;
+\Delight\Http\ResponseHeader::remove('X-Dd75c0cffe64df82', substr('535ab646c2f8451a', 0, 4));
+assert(\Delight\Http\ResponseHeader::get('X-Dd75c0cffe64df82') === null) or exit;
 
-\Delight\Http\ResponseHeader::set(TEST_HEADER_NAME, TEST_HEADER_VALUE);
-assert(\Delight\Http\ResponseHeader::get(TEST_HEADER_NAME) === TEST_HEADER) or exit;
+\Delight\Http\ResponseHeader::set('X-Ff4d986c9f5de0b9', '846157f7e880442a');
+assert(\Delight\Http\ResponseHeader::get('X-Ff4d986c9f5de0b9') === 'X-Ff4d986c9f5de0b9: 846157f7e880442a') or exit;
 
-assert(\Delight\Http\ResponseHeader::take(TEST_HEADER_NAME) === TEST_HEADER) or exit;
-assert(\Delight\Http\ResponseHeader::get(TEST_HEADER_NAME) === null) or exit;
+assert(\Delight\Http\ResponseHeader::take('X-Ff4d986c9f5de0b9') === 'X-Ff4d986c9f5de0b9: 846157f7e880442a') or exit;
+assert(\Delight\Http\ResponseHeader::get('X-Ff4d986c9f5de0b9') === null) or exit;
 
 echo 'ALL TESTS PASSED'."\n";
